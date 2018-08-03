c6e635b8c1a4702763dd3c9981e1e556c98caa1d
Anjali Ramchandani <aramchandani@intrepidcs.com>
2018-08-03 18:18:39 +0530

    Revert "Delete credentials.h"
    This reverts commit 92eb561cf2cf45260832dc173856a836918f0a84.

---
+++ samples/google-iot/include/credentials.h
@@ -0,0 +1,23 @@
+/* Use of a "define" and not a "static const" here to be able to
+* use pre-compile concatenation on the string. */
+#define clientcredentialIOT_THING_NAME "cc3220sf"
+
+/*
+ * Port number the MQTT broker is using.
+ */
+#define clientcredentialMQTT_BROKER_PORT 8883
+
+/*
+ * Port number the Green Grass Discovery use for JSON retrieval from cloud is using.
+ */
+#define clientcredentialGREENGRASS_DISCOVERY_PORT 8443
+
+/*
+ * Wi-Fi network to join.
+ */
+#define clientcredentialWIFI_SSID       "ics-india"
+
+/*
+ * Password needed to join Wi-Fi network.
+ */
+#define clientcredentialWIFI_PASSWORD   "qz2i-x67l-sg50"
